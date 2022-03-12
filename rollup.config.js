51cdc44a5b939c1e2eb610ff58d17a17586033e4
Yudiel Curbelo <yudiel@mrglasswindows.com>
2022-03-11 22:06:59 -0500

    fix rollup

--- rollup.config.js
+++ rollup.config.js
@@ -32,8 +32,13 @@ export default [
         external: ['react', 'react-dom', 'styled-components']
     },
     {
-        input: 'dist/esm/types/index.d.ts',
-        output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+        input: 'dist/esm/types/src/index.d.ts',
+        output: [
+            {
+                file: 'dist/index.d.ts',
+                format: 'esm'
+            }
+        ],
         plugins: [dts()]
     }
 ];
