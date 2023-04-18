52ae52bb2558b6599407d989b2f02ce3641e9435
Yudiel Curbelo <yudielcurbelo@gmail.com>
2023-04-18 15:07:41 -0400

    packages updated

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
     stories: ['../stories/*.stories.@(js|jsx|ts|tsx)'],
-    addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-mdx-gfm'],
+    addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
     framework: {
         name: '@storybook/react-webpack5',
         options: {}
