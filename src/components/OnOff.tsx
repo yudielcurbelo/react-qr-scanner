9a3861e47738435f8d661b26333bd4fc4ffc1f09
YUDIEL CURBELO <yudielcurbelo@gmail.com>
2024-03-05 20:21:38 -0500

    feat: onOff component, disable button for 3 second to allows for cleanup

--- src/components/OnOff.tsx
+++ src/components/OnOff.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import CameraOff from '../assets/CameraOff';
 import CameraOn from '../assets/CameraOn';
@@ -12,9 +12,27 @@ interface ITorchProps {
 export default function OnOff(props: ITorchProps) {
     const { scanning, startScanning, stopScanning } = props;
 
+    const [buttonDisabled, setButtonDisabled] = useState(false);
+
     function toggleScanning() {
+        setButtonDisabled(true);
+
         scanning ? stopScanning?.() : startScanning();
+
+        setTimeout(() => setButtonDisabled(false), 3000);
     }
 
-    return <div style={{ bottom: 50, right: 3, position: 'absolute', zIndex: 2, cursor: 'pointer' }}>{scanning ? <CameraOff onClick={toggleScanning} /> : <CameraOn onClick={toggleScanning} />}</div>;
+    return (
+        <div
+            style={{
+                bottom: 50,
+                right: 3,
+                position: 'absolute',
+                zIndex: 2,
+                cursor: buttonDisabled ? 'default' : 'pointer'
+            }}
+        >
+            {scanning ? <CameraOff disabled={buttonDisabled} onClick={toggleScanning} /> : <CameraOn disabled={buttonDisabled} onClick={toggleScanning} />}
+        </div>
+    );
 }
