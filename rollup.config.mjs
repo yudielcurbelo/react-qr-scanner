b15be4292caf81c42ac07afee8fa1500bbfb4450
YUDIEL CURBELO <yudielcurbelo@gmail.com>
2025-02-04 11:17:37 -0500

    chore: Rollup configuration separated into cjs and esm

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,44 +1,36 @@
-import dts from 'rollup-plugin-dts';
-import copy from 'rollup-plugin-copy';
-import terser from '@rollup/plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
+import copy from 'rollup-plugin-copy';
 
-import packageJson from './package.json' assert { type: 'json' };
+import packageJson from './package.json' with { type: 'json' };
 
 export default [
     {
         input: 'src/index.ts',
-        output: [
-            {
-                file: packageJson.main,
-                format: 'cjs',
-                name: 'ReactQrScanner',
-                sourcemap: true,
-                globals: { react: 'React' },
-                exports: 'named'
-            },
-            {
-                file: packageJson.module,
-                format: 'esm',
-                name: 'ReactQrScanner',
-                sourcemap: true,
-                globals: { react: 'React' },
-                exports: 'named'
-            }
-        ],
+        output: {
+            file: packageJson.main,
+            format: 'cjs',
+            name: 'ReactQrScanner',
+            sourcemap: true,
+            globals: { react: 'React' },
+            exports: 'named'
+        },
         plugins: [
             commonjs(),
             typescript({
                 tsconfig: './tsconfig.json',
+                compilerOptions: {
+                    declaration: true,
+                    declarationDir: 'dist/cjs'
+                },
                 exclude: ['**/stories/**']
             }),
             copy({
-                targets: [
-                    { src: 'src/assets/*', dest: 'dist/assets' }
-                ]
+                targets: [{ src: 'src/assets/*', dest: 'dist/assets' }]
             }),
-            terser()],
+            terser()
+        ],
         external: [
             'react',
             'react-dom',
@@ -50,14 +42,35 @@ export default [
         ]
     },
     {
-        input: 'dist/esm/index.d.ts',
-        output: [
-            {
-                file: 'dist/index.d.ts',
-                format: 'esm',
-                chunkFileNames: '[name].js'
-            }
+        input: 'src/index.ts',
+        output: {
+            file: packageJson.module,
+            format: 'esm',
+            name: 'ReactQrScanner',
+            sourcemap: true,
+            globals: { react: 'React' },
+            exports: 'named'
+        },
+        plugins: [
+            commonjs(),
+            typescript({
+                tsconfig: './tsconfig.json',
+                compilerOptions: {
+                    declaration: true,
+                    declarationDir: 'dist/esm'
+                },
+                exclude: ['**/stories/**']
+            }),
+            terser()
         ],
-        plugins: [dts()]
+        external: [
+            'react',
+            'react-dom',
+            'barcode-detector',
+            'webrtc-adapter/dist/chrome/getusermedia',
+            'webrtc-adapter/dist/firefox/getusermedia',
+            'webrtc-adapter/dist/safari/safari_shim',
+            'webrtc-adapter/dist/utils'
+        ]
     }
 ];
