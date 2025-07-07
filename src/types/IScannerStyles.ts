47a2595bf69c38001c9418bad26badb07411bb6c
Fabio Valsecchi <fabio.valsecchi@iit.cnr.it>
2025-07-07 13:47:54 +0200

    Add files via upload

--- src/types/IScannerStyles.ts
+++ src/types/IScannerStyles.ts
@@ -3,4 +3,23 @@ import type { CSSProperties } from 'react';
 export interface IScannerStyles {
     container?: CSSProperties;
     video?: CSSProperties;
+
+    // Custom styles for the Finder component
+    borderBox?: React.CSSProperties;
+    fullContainer?: React.CSSProperties;
+    innerContainer?: React.CSSProperties;
+    overlay?: React.CSSProperties;
+
+    // Common style for all four corners
+    corners?: React.CSSProperties;
+    // Custom style for the specific corners
+    cornerTopLeft?: React.CSSProperties;
+    cornerTopRight?: React.CSSProperties;
+    cornerBottomLeft?: React.CSSProperties;
+    cornerBottomRight?: React.CSSProperties;
+
+    // Buttons
+    onOff?: React.CSSProperties;
+    torch?: React.CSSProperties;
+    zoom?: React.CSSProperties;
 }
