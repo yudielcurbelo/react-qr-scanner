10d3c6fa4e91c9260df8dae531f4675392c20f07
Yudiel Curbelo <yudielcurbelo@gmail.com>
2025-10-18 20:11:55 -0400

    refactor: improve type safety in deepEqual function

--- src/utilities/deepEqual.ts
+++ src/utilities/deepEqual.ts
@@ -3,32 +3,27 @@ import isObject from '../utilities/isObject';
 import isDateObject from './isDateObject';
 import isPrimitive from './isPrimitive';
 
-// biome-ignore lint/suspicious/noExplicitAny: <objects can be of any type>
-export default function deepEqual(object1: any, object2: any) {
-	if (isPrimitive(object1) || isPrimitive(object2)) {
-		return object1 === object2;
-	}
+export default function deepEqual(object1: unknown, object2: unknown) {
+	if (isPrimitive(object1) || isPrimitive(object2)) return object1 === object2;
 
-	if (isDateObject(object1) && isDateObject(object2)) {
+	if (isDateObject(object1) && isDateObject(object2))
 		return object1.getTime() === object2.getTime();
-	}
 
-	const keys1 = Object.keys(object1);
-	const keys2 = Object.keys(object2);
+	const obj1 = object1 as Record<string, unknown>;
+	const obj2 = object2 as Record<string, unknown>;
 
-	if (keys1.length !== keys2.length) {
-		return false;
-	}
+	const keys1 = Object.keys(obj1);
+	const keys2 = Object.keys(obj2);
+
+	if (keys1.length !== keys2.length) return false;
 
 	for (const key of keys1) {
-		const val1 = object1[key];
+		const val1 = obj1[key];
 
-		if (!keys2.includes(key)) {
-			return false;
-		}
+		if (!keys2.includes(key)) return false;
 
 		if (key !== 'ref') {
-			const val2 = object2[key];
+			const val2 = obj2[key];
 
 			if (
 				(isDateObject(val1) && isDateObject(val2)) ||
@@ -36,9 +31,8 @@ export default function deepEqual(object1: any, object2: any) {
 				(Array.isArray(val1) && Array.isArray(val2))
 					? !deepEqual(val1, val2)
 					: val1 !== val2
-			) {
+			)
 				return false;
-			}
 		}
 	}
 
